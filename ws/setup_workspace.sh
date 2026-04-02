c9f8960650daf1acdf70e68df14ccfb9d8f73607
Weidner Sebastian <s.weidner@oth-aw.de>
2026-04-02 08:50:31 +0200

    Mod: setup script

--- ws/setup_workspace.sh
+++ ws/setup_workspace.sh
@@ -3,14 +3,11 @@ COLCON_WS=~/workspaces/ur_gazebo
 mkdir -p $COLCON_WS/src
 cd $COLCON_WS
 
-# 2. Reste von 'Rolling' und alte Build-Artefakte entfernen
-# sudo apt remove -y ros-rolling-* || true
-# sudo apt autoremove -y
-# rm -rf build/ install/ log/
+# 2. ROS-Umgebung laden
+#source /opt/ros/humble/setup.bash
 
-# 3. Benötigte Tools installieren
+# 3. Paketindex aktualisieren fuer rosdep/apt
 sudo apt update
-# sudo apt install -y python3-vcstool python3-rosdep python3-colcon-common-extensions
 
 # 4. Repositories klonen (falls nicht vorhanden)
 cd $COLCON_WS/src
@@ -21,14 +18,6 @@ fi
 if [ ! -d "Universal_Robots_ROS2_Gazebo_Simulation" ]; then
     git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Gazebo_Simulation.git
 fi
-
-
-# sudo apt update
-# Explizite Installation der Core-Steuerung (als Backup zu rosdep)
-#sudo apt install -y \
-#    ros-humble-ros2-control \
-#    ros-humble-ros2-controllers \
-#    ros-humble-joint-state-broadcaster
 
 # 5. VCS Import: Holt die exakt passenden Abhängigkeiten (ur_msgs, ur_client_library etc.)
 cd $COLCON_WS
