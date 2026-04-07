662a3fc2e3c58454af97bf1bda3624534027d245
Weidner Sebastian <s.weidner@oth-aw.de>
2026-04-07 09:27:44 +0200

    Add: experimentel Gripper Setup scirpts

--- ws/.devcontainer/setup_workspace_temp.sh
+++ ws/.devcontainer/setup_workspace_temp.sh
@@ -1,42 +1,47 @@
-# #!/bin/bash
-# # set -e
-
-# # 1. Variablen definieren und Verzeichnisse sicherstellen
-# COLCON_WS=~/workspaces/ur_gazebo
-# mkdir -p $COLCON_WS/src
-# cd $COLCON_WS
-
-# # 2. ROS-Umgebung laden
-# source /opt/ros/humble/setup.bash
-
-# # 3. Paketindex aktualisieren fuer rosdep/apt
-# sudo apt update
-
-# # 4. Repositories klonen (falls nicht vorhanden)
-# cd $COLCON_WS/src
-# # if [ ! -d "Universal_Robots_ROS2_Driver" ]; then
-# #     git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Driver.git
-# # fi
-
-# if [ ! -d "Universal_Robots_ROS2_Gazebo_Simulation" ]; then
-#     git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Gazebo_Simulation.git
-# fi
-
-# # 5. VCS Import: Holt die exakt passenden Abhängigkeiten (ur_msgs, ur_client_library etc.)
-# cd $COLCON_WS
-# # vcs import src < src/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
-
-# # 6. System-Abhängigkeiten installieren
-# rosdep update
-# rosdep install --from-paths src --ignore-src -r -y
-
-# # 7. Sauberer Build
-# colcon build --symlink-install
-
-# # 8. Workspace sourcen
-# source install/setup.bash
+#!/bin/bash
+set -e
 
 
-# # Add workspace source to .bashrc for future shells
-# grep -qxF "source $COLCON_WS/install/setup.bash" ~/.bashrc || \
-#     echo "source $COLCON_WS/install/setup.bash" >> ~/.bashrc
+# 1. Variablen definieren und Verzeichnisse sicherstellen
+COLCON_WS=~/workspaces/robotiq_gripper
+mkdir -p $COLCON_WS/src
+cd $COLCON_WS
+
+# 2. ROS-Umgebung laden
+source /opt/ros/humble/setup.bash
+
+# 3. Paketindex aktualisieren fuer rosdep/apt
+sudo apt update
+
+# 4. Repositories klonen (falls nicht vorhanden)
+cd $COLCON_WS/src
+# serial wird von robotiq_driver benötigt (ROS2-kompatibler Fork)
+if [ ! -d "serial" ]; then
+    git clone -b ros2 https://github.com/tylerjw/serial.git
+fi
+
+
+# Robotiq Gripper
+if [ ! -d "ros2_robotiq_gripper" ]; then
+    git clone -b humble https://github.com/PickNikRobotics/ros2_robotiq_gripper.git
+fi
+
+
+# 5. VCS Import: Holt die exakt passenden Abhängigkeiten (ur_msgs, ur_client_library etc.)
+cd $COLCON_WS
+# vcs import src < src/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
+
+# 6. System-Abhängigkeiten installieren
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+
+# 7. Sauberer Build
+colcon build --symlink-install
+
+# 8. Workspace sourcen
+source install/setup.bash
+
+
+# Add workspace source to .bashrc for future shells
+grep -qxF "source $COLCON_WS/install/setup.bash" ~/.bashrc || \
+    echo "source $COLCON_WS/install/setup.bash" >> ~/.bashrc
